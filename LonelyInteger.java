63ca494064c9f830ff9169badf63c32c10cde852
Emanuel A. Ocampo <eocampo86@gmail.com>
2016-10-26 23:03:34 -0300

    Se corrige indentación.

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -2,14 +2,14 @@ public class LonelyInteger {
     
     public static int lonelyInteger(int[] a) {
         int result = 0;
-		    int[] counts = new int[101];
-		    if (a.length == 1){
-			      result = a[0];
-		    }
-		    else{			
-			      for (int i = 0; i < a.length; i++) {
-				        counts[a[i]] += 1;
-			      }
+	int[] counts = new int[101];
+	if (a.length == 1){
+	    result = a[0];
+	}
+	else{			
+	    for (int i = 0; i < a.length; i++) {
+	        counts[a[i]] += 1;
+	    }
             int i = 0;
             while (i < counts.length && !(counts[i] == 1)){			
                 i++;
@@ -17,17 +17,17 @@ public class LonelyInteger {
 
             if (counts[i] == 1)
               result = i;
-		    }
-		    return result;
+	}
+	return result;
     }
     
     public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            int a[] = new int[n];
-            for(int a_i=0; a_i < n; a_i++){
-                a[a_i] = in.nextInt();
-            }
-            System.out.println(lonelyInteger(a));
+        Scanner in = new Scanner(System.in);
+        final int n = in.nextInt();
+        int a[] = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = in.nextInt();
+        }
+        System.out.println(lonelyInteger(a));
     }
 }
